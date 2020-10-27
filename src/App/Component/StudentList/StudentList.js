e060eaf772cda59bd75fc4957414e7b17f159fce
daiyu <1435480344@qq.comn>
2020-10-27 21:08:56 +0800

    [daiyu] feat: save stuent

--- src/App/Component/StudentList/StudentList.js
+++ src/App/Component/StudentList/StudentList.js
@@ -10,35 +10,63 @@ export default class StudentList extends React.Component{
     }
 
     componentDidMount() {
-        const url = "http://localhost:8080/students";
-        const myHeaders = new Headers({
-          "Access-Control-Allow-Origin": "*",
-          "Content-Type": "application/json",
-        });
-        fetch(url, {
-          method: "GET",
-          headers: myHeaders,
-          mode: "cors",
-        })
-        .then(res => {
-          const data = res.json()
-          return data
-        })
-        .then(data => {
-            this.setState({
-              studentlist: data,
-            });
+       this.update()
+    }
+
+    update=()=>{
+      const url = "http://localhost:8080/students";
+      const myHeaders = new Headers({
+        "Access-Control-Allow-Origin": "*",
+        "Content-Type": "application/json",
+      });
+      fetch(url, {
+        method: "GET",
+        headers: myHeaders,
+        mode: "cors",
+      })
+      .then(res => {
+        const data = res.json()
+        return data
+      })
+      .then(data => {
+          this.setState({
+            studentlist: data,
           });
-      }
+        });
+    }
+
+    handleChange= (event) =>{
+      this.setState({
+        // eslint-disable-next-line react/no-unused-state
+        student: event.target.value,
+      })
+      
+    }
+
+    saveStudent=()=>{
+      const myHeaders = new Headers({
+        "Access-Control-Allow-Origin": "*",
+        "Content-type":"text/plain"
+      });
+    fetch(`http://localhost:8080/group?student=${this.state.student}`,{
+        method:"get",
+        headers:myHeaders,
+        mode: "no-cors",
+    })
+    .then(()=>{this.update()})
+    .catch(error=>error)
+    }
 
     render(){
         return <div>
             {this.state.studentlist.map(student => (
+             // eslint-disable-next-line react/button-has-type
              <button className="student" key={student}>
                {student}
-               </button>
+            </button>
             ))}
+            <input type="text" onChange={this.handleChange} />
+            <button onClick={this.saveStudent} type="button">保存</button>
         </div>
     }
-
 }
