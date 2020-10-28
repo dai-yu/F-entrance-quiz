8a93b4e2c845461a50e47ff596adf0f1788643af
Li <lzhou@thoughtworks.com>
2020-10-28 14:53:56 +0800

    add comments

--- src/App/Component/Group/Group.js
+++ src/App/Component/Group/Group.js
@@ -10,6 +10,8 @@ export default class Group extends React.Component{
     }
 
     update=()=>{
+      // TODO GTB-工程实践: - 建议把URL定义为常量
+      // TODO GTB-工程实践: - 建议把API请求提取到单独的util，进行一定程度的封装
         const url = "http://localhost:8080/group";
         const myHeaders = new Headers({
           "Access-Control-Allow-Origin": "*",
@@ -30,24 +32,29 @@ export default class Group extends React.Component{
             });
           });
       }
-  
+
+    // TODO GTB-知识点: * 这里没有必要写成两个方法
     group=()=>{
         this.update()
     }
 
     render(){
+      // TODO GTB-知识点: - 不应该由前端来排序
       let index =0;
+      // TODO GTB-工程实践: - 代码没有格式化
         return <div className="group">
         <div className="Titles"><span>分组列表</span></div>
         <button type="button" onClick={this.group} className="but">分组</button>
         <div className="content">{this.state.group.map(list => (
-             // eslint-disable-next-line react/button-has-type
              <div className="groupTitle" key={list}>
+               {/* TODO GTB-知识点: - 没有必要用indexOf，了解一些数组的map方法如何获取当前index */}
                <span className="title">{`${this.state.group.indexOf(list)+1} 组`}</span>
                <div className="list">
                {list.map(student =>{
                  index += 1;
                  return (
+                   // TODO GTB-知识点: - class命名不合理，语义和规则都有问题，不建议使用驼峰，使用中横线，另外这里跟group 有什么关系？
+                   // TODO GTB-知识点: - html标签使用不合理，div为什么type设置成button?
                   <div type="button" className="studentgroup">{`${index}.${student}`}</div>
               )
                })}
--- src/App/Component/StudentList/StudentList.js
+++ src/App/Component/StudentList/StudentList.js
@@ -5,6 +5,7 @@ export default class StudentList extends React.Component{
     constructor(props){
         super(props);
         this.state = {
+            // TODO GTB-工程实践: - 变量命名不合规
             studentlist: ["xiaom"],
         };
     }
@@ -14,6 +15,7 @@ export default class StudentList extends React.Component{
     }
 
     update=()=>{
+      // TODO GTB-工程实践: - 把API请求进行一定程度的封装以复用
       const url = "http://localhost:8080/students";
       const myHeaders = new Headers({
         "Access-Control-Allow-Origin": "*",
@@ -47,6 +49,7 @@ export default class StudentList extends React.Component{
         "Content-type":"text/plain"
       });
     if(this.state.student!==undefined){
+      // TODO GTB-工程实践: - 新增学生为什么用get请求?
       fetch(`http://localhost:8080/save?student=${this.state.student}`,{
         method:"get",
         headers:myHeaders,
@@ -65,17 +68,21 @@ export default class StudentList extends React.Component{
     }
 
     render(){
+      // TODO GTB-知识点: - id 应该由后端生成
       let index =0;
         return <div className="studentList">
           <div className="title">学员列表</div>
             {this.state.studentlist.map(student => {
              index += 1;
               return (
+                // TODO GTB-知识点: - 学生信息为什么用button ? 建议提取组件以便在Group中复用
             <button className="student" key={student} type="button">
                <span>{`${index}.${student}`}</span>
             </button>
             )
             })}
+            {/* TODO GTB-知识点:  - 没有必要在state里面保存一个student，输入完成直接拿到数据请求API 就可以了，onChang 每次输入都触发方法调用， onKeyPress是按下去的时候触发，应该用onKeyUp或者onBlur */}
+            {/* TODO GTB-知识点:  - input 不应该一直出现，需求是按钮和input 交替出现的 */}
             <input type="text" onChange={this.handleChange}  value={this.state.student} className="value" onKeyPress={this.saveStudent}/>
         </div>
     }
